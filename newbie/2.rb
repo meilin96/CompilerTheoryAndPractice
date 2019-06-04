346a8581058f779896ecf386ffa8aa921b9cea2c
meilin996 <lin.mei360@gmail.com>
2019-06-05 00:53:57 +0800

    .

---
+++ newbie/2.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Multiply.new(left.reduce, right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+end
+
+class Machine < Struct.new(:expression)
+  def step
+    self.expression = expression.reduce
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+      puts expression
+
+  end
+end
+
+a = Multiply.new(
+  Add.new(Number.new(1), Number.new(2)),
+  Add.new(Number.new(3), Number.new(4))
+)
+
+Machine.new(a).run
---
+++ newbie/README.md
@@ -0,0 +1,1 @@
+https://www.amazon.com/Understanding-Computation-Machines-Impossible-Programs/dp/1449329276
